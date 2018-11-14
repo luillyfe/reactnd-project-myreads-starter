50d79468dbeb61d50cc94eaddc84a390586f4de6
Fermin Blanco <luillyfe89@gmail.com>
2018-11-14 01:01:48 -0500

    refactor: Shelf class component to stateless component

--- src/Shelf.js
+++ src/Shelf.js
@@ -1,29 +1,27 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Book from './Book';
 
-class Shelf extends Component {
-    render() {
-        const { title, books, changeShelf } = this.props;
-        return (
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">{ title }</h2>
-                <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {books.map(
-                            book => <Book
-                                shelf={book.shelf}
-                                key={book.id}
-                                title={book.title}
-                                imageLinks={book.imageLinks}
-                                authors={book.authors}
-                                changeShelf={e => changeShelf(e.target.value, book)}/>
-                        )}
-                    </ol>
-                </div>
+const Shelf  = props  => {
+    const { title, books, changeShelf } = props;
+    return (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{ title }</h2>
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {books.map(
+                        book => <Book
+                            shelf={book.shelf}
+                            key={book.id}
+                            title={book.title}
+                            imageLinks={book.imageLinks}
+                            authors={book.authors}
+                            changeShelf={e => changeShelf(e.target.value, book)}/>
+                    )}
+                </ol>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 Shelf.propTypes = {
